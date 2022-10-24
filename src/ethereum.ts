f20ae78281bf8d31895b5ee28abce4f155c06c65
akashraj98 <akashraj7713@gmail.com>
2022-10-24 14:55:25 +0530

    added files for ethereum and polygon

---
+++ src/ethereum.ts
@@ -0,0 +1,59 @@
+let ethereum = {
+    "USDC":{
+        "name":"USD Coin",
+        "symbol": "USDC",
+        "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+    },
+    "USDT":{
+        "name":"Tether",
+        "symbol": "USDT",
+        "address": "0xdac17f958d2ee523a2206206994597c13d831ec7"
+    },
+    "BUSD":{
+        "name":"Binance USD",
+        "symbol": "BUSDC",
+        "address": "0x4fabb145d64652a948d72533023f6e7a623c7c53"
+    },
+    "DAI":{
+        "name":"Dai",
+        "symbol": "DAI",
+        "address": "0x6b175474e89094c44da98b954eedeac495271d0f"
+    },
+    "SHIB":{
+        "name":"shiba-inu",
+        "symbol": "SHIB",
+        "address": "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce"
+    },
+    "WBTC":{
+        "name":"Wrapped Bitcoin",
+        "symbol": "WBTC",
+        "address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
+    },
+    "UNI":{
+        "name":"Uniswap",
+        "symbol": "UNI",
+        "address": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
+    },
+    "FTT":{
+        "name":"FTX",
+        "symbol": "FTT",
+        "address": "0x50d1c9771902476076ecfc8b2a83ad6b9355a4c9"
+    },
+    "CRONOS":{
+        "name":"Cronos",
+        "symbol": "CRO",
+        "address": "0xa0b73e1ff0b80914ab6fe0444e65848c4c34450b"
+    },
+    "QNT":{
+        "name":"Quant",
+        "symbol": "QNT",
+        "address": "0x4a220e6096b25eadb88358cb44068a3248254675"
+    },
+    "XCN":{
+        "name":"Chain",
+        "symbol": "XCN",
+        "address": "0xa2cd3d43c775978a96bdbf12d733d5a1ed94fb18"
+    }
+
+}
+export{ethereum}
---
+++ src/polygon.ts
@@ -0,0 +1,34 @@
+let polygon = {
+    "USDT":{
+        "name":"Tether",
+        "symbol": "USDT",
+        "address": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
+    },
+    "WMATIC":{
+        "name":"Wrapped Matic",
+        "symbol": "WMATIC",
+        "address": "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"
+    },
+    "SUSHI":{
+        "name":"Sushi",
+        "symbol": "SUSHI",
+        "address": "0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a"
+    },
+    "BLOK":{
+        "name":"Bloktopia",
+        "symbol": "BLOK",
+        "address": "0x229b1b6c23ff8953d663c4cbb519717e323a0a84"
+    },
+    "GMEE":{
+        "name":"GAMEE",
+        "symbol": "GMEE",
+        "address": "0xcf32822ff397ef82425153a9dcb726e5ff61dca7"
+    },
+    "MODEFI":{
+        "name":"Modefi",
+        "symbol": "MODEFI",
+        "address": "0x8346ab8d5ea7a9db0209aed2d1806afa0e2c4c21"
+    }
+}
+
+export{polygon}
